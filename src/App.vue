b7e82813520446d4110d522280f16951b7d0bb88
ssouron <stef@vertcitron.fr>
2018-12-10 10:02:52 +0100

    added AddTask component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,70 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>Todo List</header>
+    <add-task></add-task>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import AddTask from './components/AddTask'
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
+  components: { AddTask }
 }
 </script>
 
 <style lang="scss">
+body {
+  background-color: #888;
+  height: 100%;
+  font-family: sans-serif;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: #fafafa;
+  color: #444;
+  margin: 60px auto 48px auto;
+  width: 75%;
+  border-radius: 6px;
+  padding: 12px;
+  header {
+    font-size: 24px;
+    font-weight: bold;
+    border-bottom: 1px solid #888;
+    padding-bottom: 12px;
+  }
+  .line {
+    margin-left: auto;
+    margin-right: auto;
+    width: 75%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  }
+  button {
+    border: none;
+    padding: 8px 16px;
+    border-radius: 6px;
+    box-shadow: 2px 2px 8px rgba(black, 0.33);
+    background-color: #4682b4;
+    color: white;
+    font-weight: bold;
+    opacity: 0.85;
+    &:hover {
+      opacity: 1;
+      cursor: pointer;
+      box-shadow: 2px 2px 8px rgba(black, 0.5);
+    }
+  }
+  input[type=text] {
+    line-height: 24px;
+    border: none;
+    border-bottom: 1px solid #888;
+    background-color: inherit;
+    &:focus {
+      background-color: white;
+    }
+  }
 }
 </style>
---
+++ src/components/AddTask.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="line add-task">
+    <input class="wide" type="text" v-model="newTask" />
+    <button @click="addTask">Ajouter</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AddTask',
+  data () {
+    return {
+      newTask: ''
+    }
+  },
+  methods: {
+    addTask () {
+      if (this.newTask) this.$emit('input', this.newTask)
+      this.newTask = ''
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.add-task {
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
+.wide {
+  margin-right: 32px;
+  flex: 1;
+}
+</style>
